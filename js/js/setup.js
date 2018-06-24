7b287a2943febc9cc8c0da26247110ed50899664
Tatoshka <tapolapa@gmail.com>
2018-06-24 12:32:37 +0300

    ошибки

--- js/js/setup.js
+++ js/js/setup.js
@@ -36,3 +36,48 @@ wizards.forEach(function (item) {
 
 var setupSimilar = document.querySelector('.setup-similar');
 setupSimilar.classList.remove('hidden');
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var setupBlock = document.querySelector('.setup');
+var setupOpenBlock = document.querySelector('.setup-open');
+var inputUserName = document.querySelector('.setup-user-name');
+var setupCloseButton = document.querySelector('.setup-close');
+var setupOpenIcon = document.querySelector('.setup-open-icon');
+setupOpenIcon.setAttribute('tabindex', '0');
+setupCloseButton.setAttribute('tabindex', '0');
+
+var openPopup = function () {
+  setupBlock.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEsc);
+};
+var closePopup = function () {
+  setupBlock.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEsc);
+};
+setupOpenBlock.addEventListener('click', openPopup);
+setupOpenIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+setupCloseButton.addEventListener('click', closePopup);
+setupCloseButton.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+var onPopupEsc = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE && inputUserName !== document.activeElement) {
+    closePopup();
+  }
+};
+inputUserName.addEventListener('invalid', function (evt) {
+  if (inputUserName.validity.tooShort) {
+    inputUserName.setCustomValidity('Имя должно состоять минимум из 2 символов');
+  } else if (inputUserName.validity.tooLong) {
+    inputUserName.setCustomValidity('Имя должно состоять максимум из 25 символов');
+  } else if (inputUserName.validity.valueMissing) {
+    inputUserName.setCustomValidity('Обязательное для заполнения поле');
+  }
+});
